8b47165f555ca227615184e727b825b0a42d3de0
Ehsan Lotfinia <lotfiniamail@gmail.com>
2025-08-15 21:23:55 +0330

    fix sql

--- sql.sql
+++ sql.sql
@@ -1,10 +1,3 @@
-USE [zaermas1_db]
-GO
-/****** Object:  Table [dbo].[Traffic]    Script Date: 12/06/1403 08:52:45 ق.ظ ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
 CREATE TABLE [dbo].[Traffic](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[Barcode] [int] NOT NULL,
@@ -51,4 +44,9 @@ CREATE TABLE [dbo].[Zaer](
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+INSERT INTO [dbo].[Users] 
+    ([Username], [Password], [Fullname], [IsActive])
+VALUES 
+    ('admin', '$2a$11$pCF4869ehPyko7Az8s/l6eumvN3LfcLni50Nwc7IGzwUTA3y6nlLu', 'admin', 1)
 GO
